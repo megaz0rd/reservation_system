ac7861db44ab56b30a1a320507f6e3028a8a07ff
megaz0rd <akropiewnicki@gmail.com>
2021-01-30 13:34:52 +0100

    Created booking model

--- src/conference_room/models.py
+++ src/conference_room/models.py
@@ -5,3 +5,19 @@ class ConferenceRoom(models.Model):
     name = models.CharField(max_length=255, unique=True)
     capacity = models.PositiveSmallIntegerField()
     projector = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    room_id = models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE,
+                                related_name='booking')
+    date = models.DateTimeField()
+    comment = models.TextField()
+
+    class Meta:
+        unique_together = ('room_id', 'date')
+
+    def __str__(self):
+        return self.date
